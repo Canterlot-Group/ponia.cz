5d5a30d9dfde582719f4b9b35b5bda1c46f0eb89
noskla <alis@krutt.org>
2020-11-11 21:15:06 +0100

    Fix tests

--- test/index.js
+++ test/index.js
@@ -3,8 +3,10 @@ const request = require('supertest');
 
 describe('Basics', () => {
 
-  request(app).get('/').expect(200, (err, res) => {
-    done();
+  it('should do nothing', () => {
+    request(app).get('/').expect(200, (err, res) => {
+
+    });
   });
 
 });
