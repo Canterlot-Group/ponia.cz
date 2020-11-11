448542adf16c7e31c366ce0c18ce3fb7fdf0c52b
noskla <alis@krutt.org>
2020-11-11 21:04:06 +0100

    Replace MongoDB with SQLite/Sequelize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('bodyParser');
-const mongodb = require('mongodb').MongoClient;
+const sequelize = require('sequelize');
 const morgan  = require('morgan');
 const helmet  = require('helmet');
 const cors    = require('cors');
